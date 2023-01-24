0494fe018067ad0d15c6357818290f34d76c59e1
RecT3 <ryanectaylor@gmail.com>
2023-01-24 17:01:55 -0500

    Added

--- large-scale-programming/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ large-scale-programming/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,9 +1,13 @@
+/**
+ * Name: Ryan Taylor
+ */
+
 package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World!!");
 
 	}
 
